e21fec7898ff956cdbfb9a19e2b8280e48971665
mansi477 <mdmanasvini6@gamil.com>
2026-05-15 01:56:06 +0530

    final commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Azure devops build successfull" );
+        System.out.println( "Hello from devops final lab" );
     }
 }
